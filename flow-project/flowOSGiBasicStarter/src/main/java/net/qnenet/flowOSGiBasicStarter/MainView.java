97b409afbd8a6f67a69acd834ac78afcde913cc2
paulf <paulf@qnenet.com>
2019-02-07 09:48:40 +1100

    create flow frontend amd starter modules

---
+++ flow-project/flowOSGiBasicStarter/src/main/java/net/qnenet/flowOSGiBasicStarter/MainView.java
@@ -0,0 +1,32 @@
+package net.qnenet.flowOSGiBasicStarter;
+
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+// Need to explicitly declare the Lumo until https://github.com/vaadin/flow/issues/4847 is fixed
+@Theme(Lumo.class)
+//@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
+public class MainView extends VerticalLayout {
+
+	public MainView() {
+		Button button = new Button("Click me", event -> Notification.show("Clicked!"));
+
+//		TextField textField = new TextField();
+//		Label greeting = new Label("Hello Stranger");
+//		
+//		textField.addValueChangeListener(event -> {
+//			
+//		});
+
+		add(button);
+	}
+}
